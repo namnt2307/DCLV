f1db262dd26f62d5f62f53e6b3fd34574102d496
nam nguyen <nam.nguyen.tuan@dzones.vn>
2022-01-04 16:34:19 +0700

    update shell disease.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ export DJANGO_SUPERUSER_PASSWORD=12345678
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_EMAIL=namnt96@fpt.com.vn
 
-/usr/bin/python3 manage.py createsuperuser --noinput
+python3 manage.py createsuperuser --noinput
 
-python3 fhir/disease.py
+python3 manage.py shell < fhir/disease.py
